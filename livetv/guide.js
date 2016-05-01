3e9b390a18d751a5c01a1f768208c89bdfc09886
denz2 <djelicic@outlook.com.au>
2016-05-01 16:32:14 +0800

    Update guide.js

--- livetv/guide.js
+++ livetv/guide.js
@@ -47,7 +47,7 @@ define(['tvguide', 'events', 'datetime', 'imageLoader', 'backdrop'], function (t
                 html += '<iron-icon icon="hd"></iron-icon>';
            }
             if (item.IsMovie) {
-                 html += '<div style="display:flex;align-items:center;">';
+                 html += '<div style="display:flex;align-items:left;">';
                   html += '<h2>' + "Movie:" + '</h2>';
            }
 
