17103e113ffb4e57a1373e0baf70178b80ad35ad
Jdiesel87 <jdereniwski@gmail.com>
2016-07-05 12:38:22 -0600

    Update tv.js

--- tv/tv.js
+++ tv/tv.js
@@ -140,7 +140,7 @@ define(['loading', './../skininfo', 'alphaPicker', './../components/horizontalli
                 },
                 autoFocus: autoFocus,
                 cardOptions: {
-                    shape: 'bannerCard',
+                    shape: 'banner1Card',
                     rows: 3,
                     indexBy: 'PremiereDate'
                 },
