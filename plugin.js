29416c74948f2ded58239634601c7942ad9b907b
KevinHughesPhotography <kevinhughesphotography@gmail.com>
2017-07-09 01:37:40 -0400

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -17,9 +17,9 @@ define(['playbackManager', 'pluginManager', 'browser', 'connectionManager', 'eve
 
         var self = this;
 
-        self.name = 'Default Skin';
+        self.name = 'Smoothie';
         self.type = 'skin';
-        self.id = 'defaultskin';
+        self.id = 'smoothie';
 
         var settingsObjectName = self.id + '/skinsettings';
 
